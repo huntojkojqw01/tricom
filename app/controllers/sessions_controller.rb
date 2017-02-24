12407774b73d422f5ce1847323ef10d54e019ff0
anhmt <maitrunganh212@gmail.com>
2017-02-24 09:56:18 +0700

    fix bug

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,7 +10,7 @@ class SessionsController < ApplicationController
   def confirm_mail
     user = User.find_by(担当者コード: params[:session][:担当者コード], email: params[:session][:email])
 
-    # if user
+    if user
     #   options = { :address              => "smtp.gmail.com",
     #               :port                 => 587,
     #               :domain               => 'heroku.com',
