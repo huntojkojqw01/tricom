7dc9bcca2509d986aad26be5f8567d51f640fce7
anhmt <maitrunganh212@gmail.com>
2017-03-17 13:00:24 +0700

    fix migration

--- db/migrate/20170317040004_create_conversations.rb
+++ db/migrate/20170317040004_create_conversations.rb
@@ -1,7 +1,5 @@
 class CreateConversations < ActiveRecord::Migration[5.0]
   def change
-    drop_table :messages
-    drop_table :conversations
     create_table :conversations do |t|
       t.string :sender_id
       t.string :recipient_id
