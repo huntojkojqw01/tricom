9df407784ce49534bcc6294817fb0479e4bcb0ca
huntojkojqw01 <hungnd.k58@gmail.com>
2017-06-19 14:16:19 +0700

    test new+ edit

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,6 +5,17 @@ require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+require "capybara/rails"
+require "capybara/rspec"
+require "capybara/poltergeist" # Add this line to require poltergeist
+
+# Specs flagged with `js: true` will use Capybara's JS driver. Set
+# that JS driver to :poltergeist
+Capybara.javascript_driver = :poltergeist
+options = {js_errors: false}
+Capybara.register_driver :poltergeist do |app|
+  Capybara::Poltergeist::Driver.new(app, options)
+end
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
