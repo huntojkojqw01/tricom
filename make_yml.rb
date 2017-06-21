19e59900b11e0e354375859113f81d766b17df62
anhmt <maitrunganh212@gmail.com>
2017-06-21 08:02:30 +0700

    write test controller bashomaster

--- make_yml.rb
+++ make_yml.rb
@@ -1,18 +1,20 @@
 table=ARGV[0]
 filename=ARGV[1]
 if table!=nil && filename!=nil && filename!=""
-	filename+=".yml" unless filename[-4..-1]==".yml"
-	table=table.capitalize	
-		f=File.open("spec/fixtures/#{filename}","w")	
-		table.constantize.all.each.with_index do |element,i|		
-			f.write "#{table.downcase}_#{i}:\n"
-			table.constantize.column_names.each.with_index do |name,i|
-				f.write " #{name}: #{element.send(name)}\n"
-			end
-		end
-		f.close	
+    filename+=".yml" unless filename[-4..-1]==".yml"
+    table=table.capitalize
+        f=File.open("spec/fixtures/#{filename}","w")
+        table.constantize.all.each.with_index do |element,i|
+            f.write "#{table.downcase}_#{i}:\n"
+            table.constantize.column_names.each.with_index do |name,i|
+                if table.constantize.column_for_attribute(name).type== "string".to_sym
+                    f.write " #{name}: \"#{element.send(name)}\"\n"
+                else
+                    f.write " #{name}: #{element.send(name)}\n"
+                end
+            end
+        end
+        f.close
 else
-	print "Wrong number argument( syntax: rails r make_yml.rb TABLENAME FILENAME)\n"
+    print "Wrong number argument( syntax: rails r make_yml.rb TABLENAME FILENAME)\n"
 end
-
-
--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -1,11 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe EventsController, type: :controller do
-
+  render_views
+  before :each do
+    user= User.find(10029)
+    session[:user] = user.id
+    session[:current_user_id] = user.id
+    session[:selected_shain] = user.shainmaster.id
+  end
   describe "GET #index" do
-    it "returns http success" do
-      get :index_old
-      expect(response).to have_http_status(:success)
+    it "has a 200 status code" do
+      get :index
+      expect(response.status).to eq(200)
+    end
+    it "renders the index template" do
+      get :index
+      expect(response).to render_template("index")
+    end
+  end
+
+  describe "time_line_view" do
+    it "renders an empty time_line_view template" do
+      get :time_line_view
+      expect(response).to render_template("time_line_view")
     end
   end
 
